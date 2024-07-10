cbff83a0963bf40dd3e0a4852d0ac95bc7ae9984
Foysal Rahman <fr25foysal@gmail.com>
2024-07-10 19:52:01 +0600

    Navbar menu

--- app.js
+++ app.js
@@ -1,133 +1,31 @@
-/* -----------------------------------------------
-/* How to use? : Check the GitHub README
-/* ----------------------------------------------- */
+const navBtn = document.querySelector(".nav-btn")
+const shareBtn = document.querySelector(".share-btn")
+const reportBtn = document.querySelector(".report-btn")
+const navContainer = document.querySelector(".navContainer")
+const navMenu = document.querySelector(".nav-menu")
 
-/* To load a config file (particles.json) you need to host this demo (MAMP/WAMP/local)... */
-/*
-particlesJS.load('particles-js', 'particles.json', function() {
-  console.log('particles.js loaded - callback');
-});
-*/
+let isNavbarOpen = false;
 
-/* Otherwise just put the config content (json): */
+navBtn.addEventListener("click", (a) => {
+  if (isNavbarOpen) {
+    // Close the navigation bar
+    navContainer.classList.remove("bg-white", "p-5", "rounded-xl");
+    reportBtn.classList.remove("bg-[#2042B6]", "p-3");
+    shareBtn.classList.remove("bg-[#AFCD80]", "border-0");
+    navBtn.innerHTML = `<i class="fa-solid fa-bars z-10"></i>`; // Change the icon to a hamburger menu
+    navBtn.classList.remove("text-black"); // Remove the black text color
+    navMenu.classList.add("hidden"); // Hide the navigation menu
 
-particlesJS('particles-js',
-  
-  {
-    "particles": {
-      "number": {
-        "value": 80,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": {
-        "value": "#ffffff"
-      },
-      "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
-        }
-      },
-      "opacity": {
-        "value": 0.5,
-        "random": false,
-        "anim": {
-          "enable": false,
-          "speed": 1,
-          "opacity_min": 0.1,
-          "sync": false
-        }
-      },
-      "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": false,
-          "speed": 40,
-          "size_min": 0.1,
-          "sync": false
-        }
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": false,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
-          "enable": false,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
-      }
-    },
-    "interactivity": {
-      "detect_on": "canvas",
-      "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "push"
-        },
-        "resize": true
-      },
-      "modes": {
-        "grab": {
-          "distance": 400,
-          "line_linked": {
-            "opacity": 1
-          }
-        },
-        "bubble": {
-          "distance": 400,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 200
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
-      }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
+  } else {
+    // Open the navigation bar
+    navContainer.classList.add("bg-white", "p-5", "rounded-xl");
+    reportBtn.classList.add("bg-[#2042B6]", "p-3");
+    shareBtn.classList.add("bg-[#AFCD80]", "border-0");
+    navBtn.innerHTML = `<i class="fa-solid fa-xmark z-10"></i>`; // Change the icon to an 'X'
+    navBtn.classList.add("text-black"); // Add the black text color
+    navMenu.classList.remove("hidden"); // Hide the navigation menu
   }
 
-);
+  // Toggle the state of the navigation bar
+  isNavbarOpen = !isNavbarOpen;
+});
